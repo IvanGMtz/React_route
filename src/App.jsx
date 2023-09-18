9cbd5025e420f8d7a6cce4b2eaf44ae8645df70e
IvanGMtz <navi.garces24@gmail.com>
2023-09-18 16:19:50 -0500

    feat: :sparkles: Add Navigate & useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import UsersPage from "./pages/UsersPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import Navbar from "./components/Navbar";
 import UserPage from "./pages/UserPage";
+
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/usuarios" element={<Navigate to="/users" />} />
           <Route path="/users" element={<UsersPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users/:id" element={<UserPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
