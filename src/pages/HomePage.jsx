42911ab0e0db8720fd624e00808064f9ecd25735
IvanGMtz <navi.garces24@gmail.com>
2023-09-18 15:46:07 -0500

    feat: :sparkles: Use link in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom'
+
+
+const userId = 10
 export default function HomePage() {
     return (
       <div>
-        <h1>Hi world!</h1>
+        <h1>Application</h1>
+        <Link to={`/users/${userId}`}>Usuarios</Link>
       </div>
     );
 }
